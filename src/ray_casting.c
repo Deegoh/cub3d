fad6bb5769f342363eb4a6c89c496dd9fb5f8c8c
Yacine Bentayeb <yacinbent@gmail.com>
2022-08-02 15:31:26 +0200

    adding textures

--- src/ray_casting.c
+++ src/ray_casting.c
@@ -6,7 +6,7 @@
 /*   By: yacinebentayeb <yacinebentayeb@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/31 19:18:50 by yacinebenta       #+#    #+#             */
-/*   Updated: 2022/08/01 22:17:25 by yacinebenta      ###   ########.fr       */
+/*   Updated: 2022/08/02 02:42:15 by yacinebenta      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,47 +20,47 @@ int	get_texture_color(t_data *data, t_ray *ray, int y, int size)
 	{
 		if (ray->side == 'N')
 			color = my_mlx_pixel_get(&data->t[0],
-					((int)ray->x % data->map->tile_size) * 512
-					/ data->map->tile_size, y * 512 / size);
+					floor(((int)ray->x % data->map->tile_size) * 512
+						/ data->map->tile_size), floor(y * 512 / size));
 		else if (ray->side == 'W')
 			color = my_mlx_pixel_get(&data->t[3],
-					((int)ray->x % data->map->tile_size) * 512
-					/ data->map->tile_size, y * 512 / size);
+					floor(((int)ray->x % data->map->tile_size) * 512
+						/ data->map->tile_size), floor(y * 512 / size));
 		else if (ray->side == 'E')
 			color = my_mlx_pixel_get(&data->t[2],
-					((int)ray->x % data->map->tile_size) * 512
-					/ data->map->tile_size, y * 512 / size);
+					floor(((int)ray->x % data->map->tile_size) * 512
+						/ data->map->tile_size), floor(y * 512 / size));
 		else if (ray->side == 'S')
 			color = my_mlx_pixel_get(&data->t[1],
-					((int)ray->x % data->map->tile_size) * 512
-					/ data->map->tile_size, y * 512 / size);
+					floor(((int)ray->x % data->map->tile_size) * 512
+						/ data->map->tile_size), floor(y * 512 / size));
 		else
 			color = my_mlx_pixel_get(&data->t[4],
-					((int)ray->x % data->map->tile_size) * 512
-					/ data->map->tile_size, y * 512 / size);
+					floor(((int)ray->x % data->map->tile_size) * 512
+						/ data->map->tile_size), floor(y * 512 / size));
 	}
 	if (ray->ver_hor == 1)
 	{
 		if (ray->side == 'N')
 			color = my_mlx_pixel_get(&data->t[0],
-					((int)ray->y % data->map->tile_size) * 512
-					/ data->map->tile_size, y * 512 / size);
+					floor(((int)ray->y % data->map->tile_size) * 512
+						/ data->map->tile_size), floor(y * 512 / size));
 		else if (ray->side == 'W')
 			color = my_mlx_pixel_get(&data->t[3],
-					((int)ray->y % data->map->tile_size) * 512
-					/ data->map->tile_size, y * 512 / size);
+					floor(((int)ray->y % data->map->tile_size) * 512
+						/ data->map->tile_size), floor(y * 512 / size));
 		else if (ray->side == 'E')
 			color = my_mlx_pixel_get(&data->t[2],
-					((int)ray->y % data->map->tile_size) * 512
-					/ data->map->tile_size, y * 512 / size);
+					floor(((int)ray->y % data->map->tile_size) * 512
+						/ data->map->tile_size), floor(y * 512 / size));
 		else if (ray->side == '1')
 			color = my_mlx_pixel_get(&data->t[1],
-					((int)ray->y % data->map->tile_size) * 512
-					/ data->map->tile_size, y * 512 / size);
+					floor(((int)ray->y % data->map->tile_size) * 512
+						/ data->map->tile_size), floor(y * 512 / size));
 		else
 			color = my_mlx_pixel_get(&data->t[4],
-					((int)ray->y % data->map->tile_size) * 512
-					/ data->map->tile_size, y * 512 / size);
+					floor(((int)ray->y % data->map->tile_size) * 512
+						/ data->map->tile_size), floor(y * 512 / size));
 	}
 	return (color);
 }
@@ -74,7 +74,7 @@ void	display_ray(t_data *data, int x, int j)
 
 	i = 0;
 	distance = data->ray[j].delta * cos(data->ray->relative_angle);
-	size = data->map->tile_size / distance * 255;
+	size = data->map->tile_size / distance * 355;
 	// size = (SCREENHEIGHT / 2 - 1) / (distance / data->map->tile_size + 1);
 	if (data->ray[j].side == 'N' || data->ray[j].side == 'S')
 		color = make_trgb(0, 153, 0, 153);
